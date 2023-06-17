c189a1adc52ff8d9fdf2e12c5bff2d8ad2c16477
Cengizhan <182803014@ogr.cbu.edu.tr>
2023-06-18 00:13:34 +0300

    patch and delete method added

--- classes/ConstructionStages.php
+++ classes/ConstructionStages.php
@@ -67,4 +67,71 @@ class ConstructionStages
 		]);
 		return $this->getSingle($this->db->lastInsertId());
 	}
+
+
+    public function patch($id, ConstructionStagesUpdate $data)
+    {
+        $constructionStage = $this->getSingle($id);
+        foreach ($data as $key => $value) {
+            if ($key === 'status') {
+                if (!in_array($value, ['NEW', 'PLANNED', 'DELETED'])) {
+                    http_response_code(400);
+                    echo json_encode(array('message' => 'Invalid status value'));
+                    return;
+                }
+                $constructionStage[0][$key] = $value;
+            } else {
+                $constructionStage[0][$key] = $value;
+            }
+        }
+
+        $stmt = $this->db->prepare("
+        UPDATE construction_stages
+        SET name = :name,
+            start_date = :start_date,
+            end_date = :end_date,
+            duration = :duration,
+            durationUnit = :durationUnit,
+            color = :color,
+            externalId = :externalId,
+            status = :status
+        WHERE ID = :id
+    ");
+        $stmt->execute([
+            'id' => $id,
+            'name' => $constructionStage[0]['name'],
+            'start_date' => $constructionStage[0]['startDate'],
+            'end_date' => $constructionStage[0]['endDate'],
+            'duration' => $constructionStage[0]['duration'],
+            'durationUnit' => $constructionStage[0]['durationUnit'],
+            'color' => $constructionStage[0]['color'],
+            'externalId' => $constructionStage[0]['externalId'],
+            'status' => $constructionStage[0]['status'],
+        ]);
+
+        // Return a success response
+        http_response_code(200);
+        echo json_encode(array('message' => 'Construction stage updated successfully'));
+    }
+
+    public function delete($id)
+    {
+        $constructionStage = $this->getSingle($id);
+        if (empty($constructionStage)) {
+            http_response_code(404);
+            echo json_encode(array('message' => 'Construction stage not found'));
+            return;
+        }
+        $constructionStage['status'] = 'DELETED';
+        $this->patch($id, $constructionStage);
+
+        // Başarılı yanıtı döndür
+        http_response_code(200);
+        echo json_encode(array('message' => 'Construction stage deleted successfully'));
+    }
+
+
+
+
+
 }
